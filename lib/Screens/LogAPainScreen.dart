2756ca617c5a65850877079953fecb12bbcb1ebd
Gabriella Aguilar <k1816028@kingston.ac.uk>
2020-11-23 13:21:52 +0000

    Log Pain Works maybe a third of the time

--- lib/Screens/LogAPainScreen.dart
+++ lib/Screens/LogAPainScreen.dart
@@ -2,148 +2,157 @@ import 'package:flutter/material.dart';
 import 'package:tracker/Classes/SymptomClass.dart';
 import 'package:tracker/colors.dart';
 
-
 class LogPain extends StatefulWidget {
   @override
   _LogPainState createState() => _LogPainState();
 }
 
 class _LogPainState extends State<LogPain> {
-  double _durationController;
-
-  double _intensityController;
-
+  double _durationController = 0;
+  double _intensityController = 0;
   TextEditingController _locationController;
-
   TextEditingController _interventionController;
-
   TextEditingController _commentController;
 
   Widget build(BuildContext context) {
     Symptom symptom = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(
-                Icons.arrow_back,
-                color: backBlue,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            );
-          },
-        ),
-        backgroundColor: newBlue,
-        title: Text(
-          symptom.getName(),
-          style: TextStyle(color: backBlue),
-        ),
-      ),
-      backgroundColor: backBlue,
-      body: SafeArea(
-          child: ListView(
-              children: _listBuilder(symptom)
-          )
-      )
-
-    );
-  }
-
-  List _listBuilder(Symptom s) {
-    List<ListTile> tiles = new List<ListTile>();
-
-    if (s.getDuration()) {
-      if (_durationController == null) {
-        _durationController = 0;
-      }
-      ListTile duration = new ListTile(
-        title: Text("Duration"),
-        trailing: Slider(
-          value: _durationController,
-          activeColor: newBlueAccent,
-          inactiveColor: newBlue,
-          min: 0,
-          max: 120,
-          divisions: 5,
-          label: _durationController.round().toString() + ' minutes',
-          onChanged: (double value) {
-            setState(() {
-              _durationController = value;
-            });
-          },
-        ),
-      );
-      tiles.add(duration);
-    }
-    if (s.getIntensity()) {
-      if (_intensityController == null) {
-        _intensityController = 0;
-      }
-      ListTile intensity = new ListTile(
-        title: Text("Intensity"),
-        trailing: Slider(
-          value: _intensityController,
-          activeColor: newBlueAccent,
-          inactiveColor: newBlue,
-          min: 0,
-          max: 120,
-          divisions: 5,
-          label: _intensityController.round().toString(),
-          onChanged: (double value) {
-            setState(() {
-              _intensityController = value;
-            });
-          },
-        ),
-      );
-      tiles.add(intensity);
-    }
-    if (s.getIntervention()) {
-      ListTile intervention = new ListTile(
-        title: Text("Intervention"),
-        trailing: TextField(
-          controller: _interventionController,
-          //keyboardType: TextInputType.multiline,
-          style: TextStyle(
-            fontSize: 18,
+        appBar: AppBar(
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: backBlue,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              );
+            },
           ),
-          //minLines: 6,
-         // maxLines: 10,
-        ),
-      );
-      tiles.add(intervention);
-    }
-    if (s.getLocation()) {
-      ListTile location = new ListTile(
-        title: Text("Location"),
-        trailing: TextField(
-          controller: _locationController,
-          //keyboardType: TextInputType.multiline,
-          style: TextStyle(
-            fontSize: 18,
+          backgroundColor: newBlue,
+          title: Text(
+            symptom.getName(),
+            style: TextStyle(color: backBlue),
           ),
-          minLines: 1,
-          //maxLines: 10,
         ),
-      );
-      tiles.add(location);
-    }
-    ListTile comment = new ListTile(
-      title: Text('Comments'),
-      trailing: TextField(
-        controller: _commentController,
-        keyboardType: TextInputType.multiline,
-        style: TextStyle(
-          fontSize: 18,
-        ),
-        minLines: 6,
-        maxLines: 10,
-      ),
-    );
-    tiles.add(comment);
-    return tiles;
+        backgroundColor: backBlue,
+        body: SafeArea(
+            child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+                children: [
+                  Text("Duration "+ symptom.getDuration().toString()),
+                  Text("Intensity "+ symptom.getIntensity().toString()),
+                  Text("Intervention "+ symptom.getIntervention().toString()),
+                  Text("Location "+ symptom.getLocation().toString()),
+                  Container(
+                    height: symptom.getDuration() ? 50 : 0,
+                    child: Row(
+                      children: [
+                        Text(
+                          "Duration",
+                          style: basicText,
+                        ),
+                        Slider(
+                          value: _durationController,
+                          activeColor: newBlueAccent,
+                          inactiveColor: newBlue,
+                          min: 0,
+                          max: 120,
+                          divisions: 5,
+                          label:
+                              _durationController.round().toString() + ' minutes',
+                          onChanged: (double value) {
+                            setState(() {
+                              _durationController = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: symptom.getDuration() ? 50 : 0,
+                    child: Row(
+                      children: [
+                        Text("Intensity", style: basicText),
+                        Slider(
+                          value: _intensityController,
+                          activeColor: newBlueAccent,
+                          inactiveColor: newBlue,
+                          min: 0,
+                          max: 120,
+                          divisions: 5,
+                          label: _intensityController.round().toString(),
+                          onChanged: (double value) {
+                            setState(() {
+                              _intensityController = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: symptom.getDuration() ? 90 : 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Intervention", style: basicText),
+                        TextField(
+                          controller: _interventionController,
+                          //keyboardType: TextInputType.multiline,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                          //minLines: 6,
+                          // maxLines: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: symptom.getDuration() ? 90 : 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Location", style: basicText),
+                        TextField(
+                          controller: _locationController,
+                          //keyboardType: TextInputType.multiline,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                          minLines: 1,
+                          //maxLines: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Comments', style: basicText),
+                      TextField(
+                        controller: _commentController,
+                        keyboardType: TextInputType.multiline,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                        minLines: 6,
+                        maxLines: 10,
+                      ),
+                    ],
+                  ),
+                  RaisedButton(
+                      elevation: 8.0,
+                      child: Text('Submit'),
+                      textColor: backBlue,
+                      color: newBlue,
+                      onPressed: () {
+                        
+                      }),
+            ])));
   }
 }
