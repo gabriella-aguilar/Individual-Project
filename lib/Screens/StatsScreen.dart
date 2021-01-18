1cd009822bd684e9006e489dd4435512bb2468f7
Gabriella Aguilar <k1816028@kingston.ac.uk>
2021-01-18 20:55:50 +0000

    Update StatsScreen.dart

--- lib/Screens/StatsScreen.dart
+++ lib/Screens/StatsScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tracker/Classes/TrackingClass.dart';
 import 'package:tracker/colors.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:tracker/Screens/HomePageScreen.dart';
@@ -10,7 +11,6 @@ import '../Context.dart';
 import '../DataAccess.dart';
 
 class StatsPage extends StatefulWidget {
-
   @override
   _StatsPageState createState() => _StatsPageState();
 }
@@ -30,109 +30,30 @@ class _StatsPageState extends State<StatsPage> {
     _setUpStats();
   }
 
-  _setUpStats() async{
-    Map <DateTime,List> events = new Map<DateTime, List>();
-    final _today = DateTime.now();
-
-    Map <DateTime,List>days = await DataAccess.instance.getLoggedForCalendar();
-    Map <DateTime,List>meals = await DataAccess.instance.getMealsForCalendar();
-    Map <DateTime,List>activities = await DataAccess.instance.getActivitiesForCalendar();
-
-    if (meals == null) {
-      print("meals null");
-    } else if (meals.isEmpty) {
-      print("meals empty");
-    } else {
-      meals.forEach((key, value) {
-        bool found = false;
-        events.forEach((k, v) {
-          if (key.day == k.day &&
-              key.month == k.month &&
-              key.year == k.year) {
-            found = true;
-            List cur = new List();
-            cur.addAll(events[k]);
-            cur.addAll(value);
-            events[k] = cur;
-          }
-        });
-
-        if (!found) {
-          events[key] = value;
-        }
-        //meals[key].forEach((row) => print("Meal: " + row.getName()));
-      });
-    }
-
-    if (activities == null) {
-      print("exercise null");
-    } else if (activities.isEmpty) {
-      print("exercise empty");
-    } else {
-      activities.forEach((key, value) {
-        bool found = false;
-        events.forEach((k, v) {
-          if (key.day == k.day &&
-              key.month == k.month &&
-              key.year == k.year) {
-            found = true;
-            List cur =  new List();
-            cur.addAll(events[k]);
-            cur.addAll(value);
-            events[k] = cur;
-          }
-        });
-
-        if (!found) {
-          events[key] = value;
-        }
-        //activities[key].forEach((row) => print("Activity: " + row.getTitle()));
-      });
-    }
-    
-
-
-
-
-    final barGroup1 = makeGroupData(0, 5, 12);
-    final barGroup2 = makeGroupData(1, 16, 12);
-    final barGroup3 = makeGroupData(2, 18, 5);
-    final barGroup4 = makeGroupData(3, 20, 16);
-    final barGroup5 = makeGroupData(4, 17, 6);
-    final barGroup6 = makeGroupData(5, 19, 1.5);
-    final barGroup7 = makeGroupData(6, 10, 1.5);
-
-    final items = [
-      barGroup1,
-      barGroup2,
-      barGroup3,
-      barGroup4,
-      barGroup5,
-      barGroup6,
-      barGroup7,
-    ];
-
-    setState(() {
-      rawBarGroups = items;
-
-      showingBarGroups = rawBarGroups;
-    });
-
-  }
-
-  BarChartGroupData makeGroupData(int x, double y1, double y2) {
-    return BarChartGroupData(barsSpace: 4, x: x, barRods: [
-      BarChartRodData(
-        y: y1,
+  BarChartGroupData makeGroupData(int x, List<double> ys) {
+    List<BarChartRodData> data = new List<BarChartRodData>();
+    ys.forEach((element) {
+      data.add(new BarChartRodData(
+        y: element,
         colors: [newBlue],
         width: width,
-      ),
-      BarChartRodData(
-        y: y2,
-        colors: [darkBlueAccent],
-        width: width,
-      ),
-    ]);
+      ));
+    });
+    return BarChartGroupData(barsSpace: 4, x: x,
+        barRods: data
+    //     [
+    //   BarChartRodData(
+    //     y: y1,
+    //     colors: [newBlue],
+    //     width: width,
+    //   ),
+    //   BarChartRodData(
+    //     y: y2,
+    //     colors: [darkBlueAccent],
+    //     width: width,
+    //   ),
+    // ]
+        );
   }
 
   Widget build(BuildContext context) {
@@ -151,14 +72,17 @@ class _StatsPageState extends State<StatsPage> {
           },
         ),
         backgroundColor: newBlue,
-        title: Text('Stats Page',style: TextStyle(color: backBlue),),
+        title: Text(
+          'Stats Page',
+          style: TextStyle(color: backBlue),
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         color: newBlue,
         elevation: 2,
-        child:ButtonBar(
+        child: ButtonBar(
           alignment: MainAxisAlignment.spaceAround,
-          buttonPadding: EdgeInsets.only(bottom: 15,top: 15),
+          buttonPadding: EdgeInsets.only(bottom: 15, top: 15),
           children: <Widget>[
             FlatButton(
               child: Icon(
@@ -170,55 +94,50 @@ class _StatsPageState extends State<StatsPage> {
                 Navigator.pop(context);
                 Navigator.push(
                   context,
-                  PageRouteBuilder(
-                      pageBuilder: (_, __, ___) => HomePage()),
+                  PageRouteBuilder(pageBuilder: (_, __, ___) => HomePage()),
                 );
               },
             ),
             FlatButton(
-              child: Icon(Icons.calendar_today, size: 35, color: darkBlueAccent),
+              child:
+                  Icon(Icons.calendar_today, size: 35, color: darkBlueAccent),
               onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
                   context,
-                  PageRouteBuilder(
-                      pageBuilder: (_, __, ___) => CalendarPage()),
+                  PageRouteBuilder(pageBuilder: (_, __, ___) => CalendarPage()),
                 );
               },
             ),
             FlatButton(
               child: Icon(Icons.equalizer, size: 35, color: backBlue),
-              onPressed: () {
-
-              },
+              onPressed: () {},
             ),
             FlatButton(
-              child: Icon(Icons.account_circle,
-                  size: 35, color: darkBlueAccent),
+              child:
+                  Icon(Icons.account_circle, size: 35, color: darkBlueAccent),
               onPressed: () {
                 Navigator.pop(context);
                 Navigator.push(
                   context,
-                  PageRouteBuilder(
-                      pageBuilder: (_, __, ___) => ProfilePage()),
+                  PageRouteBuilder(pageBuilder: (_, __, ___) => ProfilePage()),
                 );
               },
             ),
           ],
         ),
-
       ),
       backgroundColor: backBlue,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-
           AspectRatio(
             aspectRatio: 1,
             child: Card(
               elevation: 0,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-              color: backBlue2,//const Color(0xff2c4260),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4)),
+              color: backBlue2, //const Color(0xff2c4260),
               child: Padding(
                 padding: const EdgeInsets.all(16),
                 child: Column(
@@ -244,7 +163,8 @@ class _StatsPageState extends State<StatsPage> {
                         ),
                         const Text(
                           'state',
-                          style: TextStyle(color: Color(0xff77839a), fontSize: 16),
+                          style:
+                              TextStyle(color: Color(0xff77839a), fontSize: 16),
                         ),
                       ],
                     ),
@@ -271,7 +191,8 @@ class _StatsPageState extends State<StatsPage> {
                                     return;
                                   }
 
-                                  touchedGroupIndex = response.spot.touchedBarGroupIndex;
+                                  touchedGroupIndex =
+                                      response.spot.touchedBarGroupIndex;
 
                                   setState(() {
                                     if (response.touchInput is FlLongPressEnd ||
@@ -283,18 +204,26 @@ class _StatsPageState extends State<StatsPage> {
                                       if (touchedGroupIndex != -1) {
                                         double sum = 0;
                                         for (BarChartRodData rod
-                                        in showingBarGroups[touchedGroupIndex].barRods) {
+                                            in showingBarGroups[
+                                                    touchedGroupIndex]
+                                                .barRods) {
                                           sum += rod.y;
                                         }
-                                        final avg =
-                                            sum / showingBarGroups[touchedGroupIndex].barRods.length;
+                                        final avg = sum /
+                                            showingBarGroups[touchedGroupIndex]
+                                                .barRods
+                                                .length;
 
                                         showingBarGroups[touchedGroupIndex] =
-                                            showingBarGroups[touchedGroupIndex].copyWith(
-                                              barRods: showingBarGroups[touchedGroupIndex].barRods.map((rod) {
-                                                return rod.copyWith(y: avg);
-                                              }).toList(),
-                                            );
+                                            showingBarGroups[touchedGroupIndex]
+                                                .copyWith(
+                                          barRods: showingBarGroups[
+                                                  touchedGroupIndex]
+                                              .barRods
+                                              .map((rod) {
+                                            return rod.copyWith(y: avg);
+                                          }).toList(),
+                                        );
                                       }
                                     }
                                   });
@@ -304,7 +233,9 @@ class _StatsPageState extends State<StatsPage> {
                               bottomTitles: SideTitles(
                                 showTitles: true,
                                 getTextStyles: (value) => const TextStyle(
-                                    color: Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 14),
+                                    color: Color(0xff7589a2),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14),
                                 margin: 20,
                                 getTitles: (double value) {
                                   switch (value.toInt()) {
@@ -330,7 +261,9 @@ class _StatsPageState extends State<StatsPage> {
                               leftTitles: SideTitles(
                                 showTitles: true,
                                 getTextStyles: (value) => const TextStyle(
-                                    color: Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 14),
+                                    color: Color(0xff7589a2),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14),
                                 margin: 32,
                                 reservedSize: 14,
                                 getTitles: (value) {
@@ -366,7 +299,6 @@ class _StatsPageState extends State<StatsPage> {
       ),
     );
   }
-
 
   Widget makeTransactionsIcon() {
     const double width = 4.5;
@@ -414,5 +346,84 @@ class _StatsPageState extends State<StatsPage> {
         ),
       ],
     );
+  }
+
+  _setUpStats() async {
+    Map<DateTime, List> events = new Map<DateTime, List>();
+    //final _today = DateTime.now();
+
+    Map<DateTime, List> days = await DataAccess.instance.getLoggedForCalendar();
+
+    List<Tracking> tracking = await DataAccess.instance.getAllTracking();
+
+    if (days.isEmpty || days == null) {
+      print("logged in Empty or null");
+    } else {
+      days.forEach((key, value) {
+        events[key] = value;
+        //print("first in logged" + value.first.getSymptom());
+      });
+
+
+      var d = [
+        DateTime.now().subtract(Duration(days: 7)),
+        DateTime.now().subtract(Duration(days: 6)),
+        DateTime.now().subtract(Duration(days: 5)),
+        DateTime.now().subtract(Duration(days: 4)),
+        DateTime.now().subtract(Duration(days: 3)),
+        DateTime.now().subtract(Duration(days: 2)),
+        DateTime.now().subtract(Duration(days: 1))
+      ];
+      var list = [
+        new List(),
+        new List(),
+        new List(),
+        new List(),
+        new List(),
+        new List(),
+        new List(),
+      ];
+      var maps =[
+        new Map<String,List>(),
+        new Map<String,List>(),
+        new Map<String,List>(),
+        new Map<String,List>(),
+        new Map<String,List>(),
+        new Map<String,List>(),
+        new Map<String,List>(),
+      ];
+
+      for(int i = 1; i <=7; i++){
+        events.forEach((key, value) {
+          if(d[i].day == key.day && d[i].month == key.month && d[i].year == key.year){
+            list[i] = value;
+          }
+        });
+      }
+
+      final barGroup1 = makeGroupData(0, [5, 12,4]);
+      final barGroup2 = makeGroupData(1, [16, 12,4]);
+      final barGroup3 = makeGroupData(2, [18, 5,4]);
+      final barGroup4 = makeGroupData(3, [20, 16,4]);
+      final barGroup5 = makeGroupData(4, [17, 6,4]);
+      final barGroup6 = makeGroupData(5, [19, 1.5,4]);
+      final barGroup7 = makeGroupData(6, [10, 1.5,4]);
+
+      final items = [
+        barGroup1,
+        barGroup2,
+        barGroup3,
+        barGroup4,
+        barGroup5,
+        barGroup6,
+        barGroup7,
+      ];
+
+      setState(() {
+        rawBarGroups = items;
+
+        showingBarGroups = rawBarGroups;
+      });
+    }
   }
 }
