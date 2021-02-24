25ab5218e8ce831ea79fe6d684d7e2cea82e3341
Gabriella Aguilar <k1816028@kingston.ac.uk>
2021-02-24 15:47:22 +0000

    Change in color scheme

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-const newBlue =  const Color(0xFF7986CB);
-const newBlue2 =  const Color(0xEE8986CB);
-const darkBlueAccent = const Color(0xFF1A237E);
-const darkBlueAccent2 = const Color(0xFF3C426B);
+const newBlue =  const Color(4285364597);
+const newBlue2 =  const Color(4286880116);
+const darkBlueAccent = const Color(4281743436);
+const darkBlueAccent2 = const Color(4284561987);
 const backBlue = Colors.white;
-const backBlue2 = Colors.white;
+const backBlue2 = const Color(4293516493);
 
 const basicText = TextStyle(
   fontSize: 18,
